1089f19147175634b02ef6e028c6f7df14034c5b
mohitmishra45 <mohitmishra9707@.com>
2025-12-04 11:45:48 +0530

    Final Fixes

--- backend/app.py
+++ backend/app.py
@@ -46,4 +46,6 @@ def get_metrics():
     return jsonify(metrics)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
